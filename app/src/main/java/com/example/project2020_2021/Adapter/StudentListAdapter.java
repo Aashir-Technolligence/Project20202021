1ecd267256c88b7849f4ac71320cfeda911b8c52
Aashir-Technolligence <aashi7651@gmail.com>
2021-01-14 16:25:40 +0500

    Initial commit

---
+++ app/src/main/java/com/example/project2020_2021/Adapter/StudentListAdapter.java
@@ -0,0 +1,63 @@
+package com.example.project2020_2021.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project2020_2021.Attribute.StudentAttr;
+import com.example.project2020_2021.R;
+
+import java.util.ArrayList;
+
+public class StudentListAdapter extends RecyclerView.Adapter<StudentListAdapter.ViewHolder> {
+    ArrayList<StudentAttr> studentAttrs;
+    private Context context;
+
+    public StudentListAdapter(ArrayList<StudentAttr> studentAttrs, Context context) {
+        this.context = context;
+        this.studentAttrs = studentAttrs;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.studentlist, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        holder.name.setText(studentAttrs.get(position).getStuname());
+        holder.gender.setText(studentAttrs.get(position).getStugender());
+        holder.phone.setText(studentAttrs.get(position).getStuphone());
+        holder.address.setText(studentAttrs.get(position).getStuaddress());
+        holder.type.setText(studentAttrs.get(position).getStutype());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentAttrs.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name, phone, gender,address,type;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.txtStuName);
+            gender = (TextView) itemView.findViewById(R.id.txtStuGender);
+            phone = (TextView) itemView.findViewById(R.id.txtStuPhone);
+            address = (TextView) itemView.findViewById(R.id.txtStuAddress);
+            type = (TextView) itemView.findViewById(R.id.txtStuType);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/project2020_2021/MainHomeActivity/HomeStuFragment.java
@@ -0,0 +1,77 @@
+package com.example.project2020_2021.MainHomeActivity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.project2020_2021.Adapter.StudentListAdapter;
+import com.example.project2020_2021.Attribute.StudentAttr;
+import com.example.project2020_2021.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HomeStuFragment extends Fragment {
+    RecyclerView listView;
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    ArrayList<StudentAttr> studentAttrs;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_home_stu, container, false);
+
+        listView = (RecyclerView) v.findViewById(R.id.recycler);
+        studentAttrs = new ArrayList<>();//constructor
+        listView.setLayoutManager(new LinearLayoutManager(getContext()));
+        database = FirebaseDatabase.getInstance();  //database connectivity
+        reference = database.getReference("Users");//table name
+
+        reference.child("Students").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(getActivity()!=null){
+                    if (dataSnapshot.exists()) {
+                        try{
+                            studentAttrs.clear();
+                            //profiledata.clear();
+                            for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                                StudentAttr p = dataSnapshot1.getValue(StudentAttr.class);
+                                studentAttrs.add(p);
+                            }
+//                            Collections.reverse(studentAttrs);
+                            listView.setAdapter(new StudentListAdapter(studentAttrs, getContext()));
+                        }
+                        catch (Exception e){}
+                    } else {
+                        Toast.makeText(getContext(), "No Record Found", Toast.LENGTH_LONG).show();
+                        }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return v;
+    }
+}
